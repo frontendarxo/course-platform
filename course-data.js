fce263e6fcbeb9c2022f4416cdbe117159dd7d1c
Salakh <mokhmadsalakh.salamov@gmail.com>
2025-11-28 14:22:34 +0300

    core: структура проекта готова

---
+++ course-data.js
@@ -0,0 +1,65 @@
+const courses = [
+    {
+        sphere: 'Marketing',
+        name: 'The Ultimate Google Ads Training Course',
+        price: '$100',
+        author: 'by Jerome Bell',
+        img: './img/first.jpg'
+    },
+    {
+        sphere: 'Management',
+        name: 'Prduct Management Fundamentals',
+        price: '$480',
+        author: 'by Marvin McKinney',
+        img: './img/second.jpg'
+    },
+    {
+        sphere: 'HR & Recruting',
+        name: 'HR  Management and Analytics',
+        price: '$200',
+        author: 'by Leslie Alexander Li',
+        img: './img/third'
+    },
+    {
+        sphere: 'Marketing',
+        name: 'Brand Management & PR Communications',
+        price: '$530',
+        author: 'by Kristin Watson',
+        img: './img/fourth.jpg'
+    },
+    {
+        sphere: 'Design',
+        name: 'Graphic Design Basic',
+        price: '$500',
+        author: 'by Guy Hawkins',
+        img: './img/fifth.jpg'
+    },
+    {
+        sphere: 'Management',
+        name: 'Business Development Management',
+        price: '$400',
+        author: 'by Dianne Russell',
+        img: './img/six.jpg'
+    },
+    {
+        sphere: 'Development',
+        name: 'Highload Software Architecture',
+        price: '$600',
+        author: 'by Brooklyn Simmons',
+        img: './img/seven.jpg'
+    },
+    {
+        sphere: 'HR & Recruting',
+        name: 'Human Resources – Selection and Recruitment',
+        price: '$150',
+        author: 'by Kathryn Murphy',
+        img: './img/pre-last'
+    },
+    {
+        sphere: 'Design',
+        name: 'User Experience. Human-centered Design',
+        price: '$240',
+        author: 'by Cody Fisher',
+        img: './img/last'
+    }
+]
